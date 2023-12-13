8a60cf4513d2416fc149d44cffbdd7f71f3cc245
Alexander Raboin <araboin@Alexanders-MacBook-Pro.local>
2023-12-12 21:25:11 -0500

    changes

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ WORKDIR /app
 
 COPY ./src .
 
+ENV INFURA_API_KEY f3c095656381439aa1acb1722d9c62f2
+
 EXPOSE 8080
 
 CMD [ "python3", "blockexplorer.py" ]
---
+++ main.tf
@@ -0,0 +1,315 @@
+# main.tf
+
+
+## variables
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for main"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type    = string
+  default = "us-east-1a"
+}
+
+
+
+## terraform aws cli access 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+
+
+
+# aws provider
+provider "aws" {
+  region  = "us-east-1" #The region where the environment 
+  #is going to be deployed # Use your own region here
+  access_key = "AKIAT3M3VHY5LFOE3KX2" # Enter AWS IAM 
+  secret_key = "VjphcjG4DozTEGtHS+mdUDS09Nu7zgknTHVazPZz" # Enter AWS IAM 
+}
+
+
+# ecr info
+
+
+
+
+# define vpc
+resource "aws_vpc" "main" {
+ cidr_block           = var.vpc_cidr
+ enable_dns_hostnames = true
+ tags = {
+  name = "blockchain_app"
+  }
+  }
+
+# Provide a reference to your default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+
+## add two subnets 
+
+resource "aws_subnet" "subnet" {
+ vpc_id                  = aws_vpc.main.id
+ cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+ map_public_ip_on_launch = true
+ availability_zone       = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+ vpc_id                  = aws_vpc.main.id
+ cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+ map_public_ip_on_launch = true
+ availability_zone       = "us-east-1b"
+}
+
+
+## internet gateway 
+
+resource "aws_internet_gateway" "internet_gateway" {
+ vpc_id = aws_vpc.main.id
+ tags = {
+   Name = "internet_gateway"
+ }
+}
+
+# routing tables
+
+resource "aws_route_table" "route_table" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.internet_gateway.id
+ }
+}
+
+resource "aws_route_table_association" "subnet_route" {
+ subnet_id      = aws_subnet.subnet.id
+ route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet2_route" {
+ subnet_id      = aws_subnet.subnet2.id
+ route_table_id = aws_route_table.route_table.id
+}
+
+
+resource "aws_security_group" "security_group" {
+ name   = "ecs-security-group"
+ vpc_id = aws_vpc.main.id
+
+ ingress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = -1
+   self        = "false"
+   cidr_blocks = ["0.0.0.0/0"]
+   description = "any"
+ }
+
+ egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
+resource "aws_launch_template" "ecs_lt" {
+ name_prefix   = "ecs-template"
+ image_id      = "ami-04c97e62cb19d53f1"
+ instance_type = "t4g.small"
+
+ key_name               = "deployer-key"
+ vpc_security_group_ids = [aws_security_group.security_group.id]
+ iam_instance_profile {
+   name = "ecsInstanceRole"
+ }
+
+ block_device_mappings {
+   device_name = "/dev/xvda"
+   ebs {
+     volume_size = 50
+     volume_type = "gp2"
+   }
+ }
+
+ tag_specifications {
+   resource_type = "instance"
+   tags = {
+     Name = "ecs-instance"
+   }
+ }
+
+ user_data = filebase64("${path.module}/ecs.sh")
+}
+
+
+resource "aws_autoscaling_group" "ecs_asg" {
+ vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+ desired_capacity    = 2
+ max_size            = 3
+ min_size            = 1
+
+ launch_template {
+   id      = aws_launch_template.ecs_lt.id
+   version = "$Latest"
+ }
+
+ tag {
+   key                 = "AmazonECSManaged"
+   value               = true
+   propagate_at_launch = true
+ }
+}
+
+
+resource "aws_lb" "ecs_alb" {
+ name               = "ecs-alb"
+ internal           = false
+ load_balancer_type = "application"
+ security_groups    = [aws_security_group.security_group.id]
+ subnets            = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+
+ tags = {
+   Name = "ecs-alb"
+ }
+}
+
+resource "aws_lb_listener" "ecs_alb_listener" {
+ load_balancer_arn = aws_lb.ecs_alb.arn
+ port              = 8080
+ protocol          = "HTTP"
+
+ default_action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+ }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 davidwinterberry@gmail.com"
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+ name        = "ecs-target-group"
+ port        = 8080
+ protocol    = "HTTP"
+ target_type = "ip"
+ vpc_id      = aws_vpc.main.id
+
+ health_check {
+   path = "/"
+ }
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+ name = "my-ecs-cluster"
+}
+
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+ name = "test1"
+
+ auto_scaling_group_provider {
+   auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+
+   managed_scaling {
+     maximum_scaling_step_size = 1000
+     minimum_scaling_step_size = 1
+     status                    = "ENABLED"
+     target_capacity           = 3
+   }
+ }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "example" {
+ cluster_name = aws_ecs_cluster.ecs_cluster.name
+
+ capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+
+ default_capacity_provider_strategy {
+   base              = 1
+   weight            = 100
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+ }
+}
+
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+ family             = "my-ecs-task"
+ network_mode       = "awsvpc"
+ execution_role_arn = "arn:aws:iam::265003810362:role/ecsTaskExecutionRole"
+ cpu                = 1024
+ runtime_platform {
+   operating_system_family = "LINUX"
+   cpu_architecture        = "X86_64"
+ }
+ container_definitions = jsonencode([
+   {
+     name      = "docker_blockchain"
+     image     = "265003810362.dkr.ecr.us-east-1.amazonaws.com/blockchain:latest"
+     cpu       = 1024
+     memory    = 1024
+     essential = true
+     portMappings = [
+       {
+         containerPort = 8080
+         hostPort      = 8080
+         protocol      = "tcp"
+       }
+     ]
+   }
+ ])
+}
+
+
+resource "aws_ecs_service" "ecs_service" {
+ name            = "my-ecs-service"
+ cluster         = aws_ecs_cluster.ecs_cluster.id
+ task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+ desired_count   = 2
+
+ network_configuration {
+   subnets         = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+   security_groups = [aws_security_group.security_group.id]
+ }
+
+ force_new_deployment = true
+ placement_constraints {
+   type = "distinctInstance"
+ }
+
+ triggers = {
+   redeployment = timestamp()
+ }
+
+ capacity_provider_strategy {
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+   weight            = 100
+ }
+
+ load_balancer {
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+   container_name   = "docker_blockchain"
+   container_port   = 8080
+ }
+
+ depends_on = [aws_autoscaling_group.ecs_asg]
+}
